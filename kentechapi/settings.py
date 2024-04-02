fe5755cdc045ef36d83620576399d10946106370
Egoba Kelvin <egobakelvin@gmail.com>
2024-04-02 18:39:17 +0100

    refactoring

--- kentechapi/settings.py
+++ kentechapi/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.getenv("DEBUG", "False") == "True"
